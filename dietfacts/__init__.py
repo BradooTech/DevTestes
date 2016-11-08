8e92609691ea257f216162f448d598bee9606792
DiogoBradoo <diogoberti88@gmail.com>
2016-11-08 11:46:15 -0200

    Alteracao

--- dietfacts/__init__.py
+++ dietfacts/__init__.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
+
 from . import controllers
-=======
-<<<<<<< HEAD
-from . import controllers
-from . import model
-=======
-from . import controller
->>>>>>> f83903c438aff711986fc75860fdf917da1a953c
 from . import models
->>>>>>> f3f8f12e3503ce9fc7d16b86c9347593268c4958
+
