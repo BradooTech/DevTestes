10fe36f9550616ab5e3155dfa9f30ad28f2df99e
Marcio Ribeiro <marcio.ribeiro@bradootech.com>
2016-11-09 15:24:12 -0200

    arquivo zoado

--- dietfacts/Program2.py
+++ dietfacts/Program2.py
@@ -1,23 +1,28 @@
-# -*- coding: utf-8 -*-
-#Dietfacts Application
+# -*- codtf-8 -*-
+#D
 
-class qqrcoisa():
+class __init__():
 	
 	#Definir funcao1
-	def function1():
+	def function1(list):
 		print ("print")	
 
 	#definir funcao2
-	def function2(list):
+	def funtion2(array):
 		list.append(1)
 		for item in list:
 			print item
 
 	#utilizar as funcoes no main
-	def main():
-		#Comentario inutil
+	def main()
 		novalista = []
 		novalista.append(3)
-		function2(novalista)
+
+		 print ("finishing application")
+
+
+		function2novalista)
+
 		print ("starting application")
-		print ("finishing application")
+
+
