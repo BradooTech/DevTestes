2d78b2cba4e9d0b4a70eadc29c6fb9c6d1166b8a
DiogoBradoo <diogoberti88@gmail.com>
2016-11-09 13:32:57 -0200

    Programa de testes

---
+++ dietfacts/Program2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#Dietfacts Application
+
+class qqrcoisa():
+	
+	#Definir funcao1
+	def function1():
+	
+
+
+	#definir funcao2
+	def function2(list):
+	
+	#utilizar as funcoes no main
+	def main():
+	
